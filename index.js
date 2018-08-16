b1a2349daa4408bcecacd2e628730066d066b817
Corneezius <cfr2116@columbia.edu>
2018-08-15 20:17:09 -0400

    fic error with clientid

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const keys = require("./config/keys");
 
-
-
-
 const app =express();
 
 passport.use(
@@ -15,6 +12,12 @@ passport.use(
     callbackURL:"/auth/google/callback"
   }, (accessToken) => {
     console.log(accessToken);
+  })
+);
+
+app.get("/auth/google",
+  passport.authenticate("google", {
+  scope: ["profile", "email"]
   })
 );
 
