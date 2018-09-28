9783e6435e4407ad9073a6489a7970af360036b1
Corneezius <cfr2116@columbia.edu>
2018-09-28 16:22:27 -0400

    update logic to determine environment

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
-module.exports = {
-  googleClientID: "866000286342-il0pv80dmikhjtg3ckfcj75o4tjge8ct.apps.googleusercontent.com",
-  googleClientSecret: "NNlqB3_gA1OFO9Wd8TSe6qnP",
-  mongoURI:"mongodb://cornelius:fivetimev5@ds117773.mlab.com:17773/emaily-dev1",
-  cookieKey:"jshfshfjsfjsgfjsg"
+if (process.env.NODE_ENV === "production") {
+module.exports =  require("./prod");
+} else {
+  module.exports = require("./dev")
 }
