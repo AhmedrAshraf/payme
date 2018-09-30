92087ce1afb0d19675f5f99f71c08822bfda6a1c
Corneezius <cfr2116@columbia.edu>
2018-09-30 12:22:40 -0400

    install stripe package and add keys

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID: "866000286342-il0pv80dmikhjtg3ckfcj75o4tjge8ct.apps.googleusercontent.com",
+  googleClientSecret: "NNlqB3_gA1OFO9Wd8TSe6qnP",
+  mongoURI:"mongodb://cornelius:fivetimev5@ds117773.mlab.com:17773/emaily-dev1",
+  cookieKey:"jshfshfjsfjsgfjsg",
+  stripePublishableKey:"pk_test_blGPz1vyaq7HDWhaMtZHsDIz",
+  stripeSecretKey:"sk_test_DoastKsLHxB8obVap0ukOXzs"
+}
