0993fee5d71eeab27136c955ae726ae7d9ed3f40
Corneezius <cfr2116@columbia.edu>
2018-09-30 13:47:33 -0400

    update payments component

--- client/src/components/payments.js
+++ client/src/components/payments.js
@@ -4,11 +4,19 @@ import StripeCheckout from "react-stripe-checkout";
 class Payments extends Component {
   render() {
     debugger;
-    
+
     return (
-      <StripeCheckout amount={500}
-      token={}
-      />
+      <StripeCheckout
+      name="Emaily"
+      description="$5.00 for 5 survey credits"
+      amount={500}
+      token={token => console.log(token)}
+      stripeKey={process.env.REACT_APP_STRIPE_KEY}
+      >
+      <button className="btn">
+        Add credits
+      </button>
+      </StripeCheckout>
     )
   }
 }
