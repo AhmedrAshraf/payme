064538e664bd6d7f50adc7fab832e1ae48999768
Corneezius <cfr2116@columbia.edu>
2018-09-30 22:32:03 -0400

    update app

--- client/src/components/landing.js
+++ client/src/components/landing.js
@@ -7,12 +7,10 @@ const Landing = () => {
       Payme!
       </h1>
       <p>This is just a test app for me to get hired as a front end or full stack developer.
-      Or if you don't wanna hire me, at least give me credit for trying, by using this dummy card
+      Or hey, at least give me credit for trying, by using this dummy card #
       to test out my app. #4242 4242 4242 4242. The other form values you can fill in with any value.
       On submit, an account is stored on MongoDB and your account is updated with 5 dummy credits.
       </p>
-
-
     </div>
   );
 }
